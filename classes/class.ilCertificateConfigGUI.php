fb09b8b17bf1b7c5b7911685e364f10e98da14a5
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2014-12-01 13:28:18 +0100

    Remove commented code

--- classes/class.ilCertificateConfigGUI.php
+++ classes/class.ilCertificateConfigGUI.php
@@ -37,73 +37,6 @@ class ilCertificateConfigGUI extends ilPluginConfigGUI
     }
 
 
-
-//    /**
-//     * @return array
-//     */
-//    public function getFields()
-//    {
-//        $this->fields = array(
-//// SW: Not yet implemented for first release
-////            'async' => array(
-////                'type' => 'ilCheckboxInputGUI',
-////                'info' => false,
-////                'subelements' => array(
-////                    'username' => array(
-////                        'type' => 'ilTextInputGUI',
-////                    ),
-////                    'password' => array(
-////                        'type' => 'ilTextInputGUI',
-////                    ),
-////                    'client' => array(
-////                        'type' => 'ilTextInputGUI',
-////                    )
-////                )
-////            ),
-////            'signature' => array(
-////                'type' => 'ilCheckboxInputGUI',
-////                'info' => false,
-////                'subelements' => array(
-////                    'privatekey' => array(
-////                        'type' => 'ilTextInputGUI',
-////                    ),
-////                    'publickey' => array(
-////                        'type' => 'ilTextInputGUI',
-////                    ),
-////                )
-////            ),
-//            'course_templates' => array(
-//                'type' => 'ilCheckboxInputGUI',
-//                'info' => true,
-//                'subelements' => array(
-//                    'ref_ids' => array(
-//                        'type' => 'ilTextareaInputGUI',
-//                        'info' => true,
-//                    ),
-//                )
-//            ),
-//            'time_format_utc' => array(
-//                'type' => 'ilCheckboxInputGUI',
-//                'info' => true,
-//            ),
-//            'str_format_date' => array(
-//                'type' => 'ilTextInputGUI',
-//                'info' => true,
-//            ),
-//            'str_format_datetime' => array(
-//                'type' => 'ilTextInputGUI',
-//                'info' => true,
-//            ),
-//            'path_hook_class' => array(
-//                'type' => 'ilTextInputGUI',
-//                'info' => true,
-//            )
-//        );
-//
-//        return $this->fields;
-//    }
-
-
     /**
      * @param $cmd
      */
@@ -136,7 +69,7 @@ class ilCertificateConfigGUI extends ilPluginConfigGUI
     {
         $form = new ilCertificateConfigFormGUI($this);
         if ($form->saveObject()) {
-            ilUtil::sendSuccess($this->pl->txt('todo'));
+            ilUtil::sendSuccess($this->pl->txt('msg_save_config'), true);
             $this->ctrl->redirect($this, 'configure');
         } else {
             $form->setValuesByPost();
@@ -144,71 +77,4 @@ class ilCertificateConfigGUI extends ilPluginConfigGUI
         }
     }
 
-
-//    public function getValues()
-//    {
-//        foreach ($this->getFields() as $key => $item) {
-//            $values[$key] = $this->object->getValue($key);
-//            if (is_array($item['subelements'])) {
-//                foreach ($item['subelements'] as $subkey => $subitem) {
-//                    $values[$key . '_' . $subkey] = $this->object->getValue($key . '_' . $subkey);
-//                }
-//            }
-//        }
-//        $this->form->setValuesByArray($values);
-//    }
-
-
-//    /**
-//     * @return ilPropertyFormGUI
-//     */
-//    public function initConfigurationForm()
-//    {
-//        global $lng, $ilCtrl;
-//        include_once('Services/Form/classes/class.ilPropertyFormGUI.php');
-//        $this->form = new ilPropertyFormGUI();
-//        foreach ($this->getFields() as $key => $item) {
-//            $field = new $item['type']($this->pl->txt($key), $key);
-//            if ($item['info']) {
-//                $field->setInfo($this->pl->txt($key . '_info'));
-//            }
-//            if (is_array($item['subelements'])) {
-//                foreach ($item['subelements'] as $subkey => $subitem) {
-//                    $subfield = new $subitem['type']($this->pl->txt($key . '_' . $subkey), $key . '_' . $subkey);
-//                    if ($subitem['info']) {
-//                        $subfield->setInfo($this->pl->txt($key . '_' . $subkey . '_info'));
-//                    }
-//                    $field->addSubItem($subfield);
-//                }
-//            }
-//            $this->form->addItem($field);
-//        }
-//        $this->form->addCommandButton('save', $lng->txt('save'));
-//        $this->form->setTitle($this->pl->txt('configuration'));
-//        $this->form->setFormAction($ilCtrl->getFormAction($this));
-//
-//        return $this->form;
-//    }
-//
-//
-//    public function save()
-//    {
-//        global $tpl, $ilCtrl;
-//        $this->initConfigurationForm();
-//        if ($this->form->checkInput()) {
-//            foreach ($this->getFields() as $key => $item) {
-//                $this->object->setValue($key, $this->form->getInput($key));
-//                if (is_array($item['subelements'])) {
-//                    foreach ($item['subelements'] as $subkey => $subitem) {
-//                        $this->object->setValue($key . '_' . $subkey, $this->form->getInput($key . '_' . $subkey));
-//                    }
-//                }
-//            }
-//            ilUtil::sendSuccess($this->pl->txt('conf_saved'));
-//            $ilCtrl->redirect($this, 'configure');
-//        } else {
-//            $this->form->setValuesByPost();
-//            $tpl->setContent($this->form->getHtml());
-//        }
-//    }
 }
