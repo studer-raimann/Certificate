c89a9c4536539274fb6fac974eaf219e5b95da16
theodortruffer <tt@studer-raimann.ch>
2015-07-15 11:44:04 +0200

    bugfix: date range of cert definition could not be copied

--- classes/Type/class.srCertificateTypeSetting.php
+++ classes/Type/class.srCertificateTypeSetting.php
@@ -147,13 +147,24 @@ class srCertificateTypeSetting extends srCertificateSetting
             case srCertificateTypeSetting::VALIDITY_TYPE_DATE_RANGE:
                 if (is_array($value) && isset($value['dd']) && isset($value['MM'])) {
                     $value = json_encode(array('d' => $value['dd'], 'm' => $value['MM']));
-                } else {
+                } else if (!self::isJson($value)){
                     $value = '';
                 }
                 break;
         }
 
         return $value;
+    }
+
+    /**
+     * return true if string is in json format
+     *
+     * @param String $string
+     * @return bool
+     */
+    public static function isJson($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
     }
 
 
