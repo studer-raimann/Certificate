048924782d1c4f4234a03980b7e0b36373a81f7d
Theodor Truffer <tt@studer-raimann.ch>
2019-02-27 14:40:55 +0100

    Version 1.7

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = 'cert';
-$version = '1.63';
+$version = '1.7';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag - Team Custom 1';
